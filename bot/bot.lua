292a59807e78edf5db811a912680ceb2bc6c4e1f
naserabbassi <32171222+naserabbassi@users.noreply.github.com>
2017-09-23 08:12:33 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -234,7 +234,7 @@ function create_config( )
 	"botmanager",
     "tabchi"
     },
-    sudo_users = {282958812},--Sudo users
+    sudo_users = {74430450},--Sudo users
   }
   serialize_to_file(config, './data/config.lua')
   print('saved config into ./data/config.lua')
